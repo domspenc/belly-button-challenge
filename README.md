ea4241d3a7bb629326737019c32f4bd2b8ae3a90
dominique <dommspencer@gmail.com>
2023-10-30 16:31:42 +0800

    updated readme file and final code

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# belly-button-challenge
+<h1>Belly Button Challenge</h1>
+<img
+        src="/img.png"
+        alt="Food Magazine rendering"
+        width="650"
+      />
+</br>
+<h3><u>Description</u></h3>
+<p>
+In this repository, JavaScript and the D3.js library was used to build an interactive dashboard to explore the Belly Button Biodiversity dataset, which catalogs the microbes that colonize human navels.
+
+The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+</p>
+</br>
+<h3><u>Instructions</u></h3>
+</br>
+<p>🦠 Use the D3 library to read in samples.json from the URL https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json</p> 
+</br>
+<p>🦠 Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.</p>
+<ol><li>Use sample_values as the values for the bar chart.</li>
+<li>Use otu_ids as the labels for the bar chart.</li>
+<li>Use otu_labels as the hovertext for the chart.</li>
+</ol> 
+</br>
+<p>🦠 Create a bubble chart that displays each sample.</p>
+<ol><li>Use otu_ids for the x values.</li>
+<li>Use sample_values for the y values.</li>
+<li>Use sample_values for the marker size.</li>
+<li>Use otu_ids for the marker colors.</li>
+<li>Use otu_labels for the text values.</li>
+</ol>  
+</br>
+<p>🦠 Display the sample metadata, i.e., an individual's demographic information.</p> 
+</br>
+<p>🦠 Display each key-value pair from the metadata JSON object somewhere on the page.</p> 
+</br>
+<p>----------------------------------------------------------</p> 
+</br>
+<p>Thank you for reading!</p> 
+</br>
+<h3>🔬🔬🔬</h3>
+</br>
+<p><a>Dataset and image sourced from http://robdunnlab.com/projects/belly-button-biodiversity/</a></p>
+</br>
--- static/js/app.js
+++ static/js/app.js
@@ -1,43 +1,143 @@
-// Use the D3 library to read in samples.json from the URL
+// Create a constant variable for the URL
 const url =
   "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-
+// Create a function that acts as a default dashboard when the webpage is opened
 function init(){
-    d3.json(url).then((data) => {
+    // Use D3 to request the data
+    d3.json(url).then((data) => { 
+        // Log response in the console
         console.log(data);
-        const dropDown = d3.select("#selDataset");
-        for(let index = 0; index < data.names.length; index++){
-            dropDown.append("option").text(data.names[index]).property("value", data.names[index])
-        }
-        buildCharts(data.names[0])
-        buildMetaData(data.names[0])
-    });
-}
-init();
+        // Create variable for drop-down menu
+        let dropDown = d3.select("#selDataset");
+        // Creat variable for names object data
+        let names = data.names;
+        // Create for loop to iterate through each name in the names variable,
+        // and append the result to the options within the drop-down menu
+        for(let i = 0; i < names.length; i++){
+            dropDown.append("option").text(names[i]).property("value", names[i])
+        };
+        // Create a variable for the first 'names' result only
+        let firstID = names[0];
+        // Log response in the console
+        console.log(firstID);
 
+        // Build the webpage start-up plots
+        buildCharts(firstID);
+        buildMetaData(firstID);
+    });
+};
+
+// --- CHARTS ---
+
+// Create an umbrella function for the bar and bubble charts
 function buildCharts(sampleId){
-    d3.json(url).then((data) => {
-        const samples = data.samples
-        const theSample = samples.filter(element => element.id == sampleId)[0]
-        console.log(theSample)
-    });
-}
+  // Use D3 to request the data
+  d3.json(url).then((data) => {
+    // Create variable to store all 'samples' data
+    let allSamples = data.samples;
+    // Create variable to store filtered samples that match required condition
+    let theSample = allSamples.filter((sample) => sample.id == sampleId);
+    // Create variable to store values from filtered sample
+    let sampleData = theSample[0];
+    // Log response in the console
+    console.log(sampleData);
 
+    // Create variables to store otu_labels, ids and values data
+    let labels = sampleData.otu_labels;
+    let ids = sampleData.otu_ids;
+    let values = sampleData.sample_values;
+
+    // Log response in the console
+    console.log(labels, ids, values);
+
+    // --- BAR CHART ---
+
+    // Create variables to store data for the x-axis, y-axis and labels
+    let xaxis = values.slice(0, 10).reverse();
+    let yaxis = ids
+      .slice(0, 10)
+      .map((otu_id) => `OTU ${otu_id}`)
+      .reverse();
+    let plotLabels = labels.slice(0, 10).reverse();
+
+    // Create trace for plotting
+    let barTrace = [
+      {
+        x: xaxis,
+        y: yaxis,
+        labels: plotLabels,
+        type: "bar",
+        orientation: "h",
+      },
+    ];
+
+    // Create plot title
+    let barLayout = {
+      title: "Top 10 OTUs",
+    };
+
+    // Call Plotly to plot bar chart
+    Plotly.newPlot("bar", barTrace, barLayout);
+
+    // --- BUBBLE CHART ---
+
+    // Create trace for plotting
+    let bubbleTrace = [
+      {
+        x: ids,
+        y: values,
+        text: labels,
+        mode: "markers",
+        marker: {
+          size: values,
+          color: ids,
+          colorscale: "Earth",
+        },
+      },
+    ];
+
+    // Create plot title
+    let bubbleLayout = {
+      title: "OTU ID",
+    };
+
+    // Call Plotly to plot bubble chart
+    Plotly.newPlot("bubble", bubbleTrace, bubbleLayout);
+  });
+};
+
+// --- METADATA ---
+
+// Create a function for Metadata
 function buildMetaData(sampleId){
-    d3.json(url).then((data) => {
-        const metadata = data.metadata;
-        const theMetadata = metadata.filter((element) => element.id == sampleId)[0];
-        console.log(theMetadata);
-        const panel = d3.select("#sample-metadata");
-        panel.html("");
-        for(key in theMetadata){
-            panel.append("p").text(key + ": " + theMetadata[key]);
-        }
-    });
-}
+  // Use D3 to request the data
+  d3.json(url).then((data) => {
+    // Create variable to store all metadata
+    let metadata = data.metadata;
+    // Create variable to store filtered items that match required condition
+    let theMetadata = metadata.filter((item) => item.id == sampleId);
+    // Create variable to store first Metadata item
+    itemID = theMetadata[0];
+    // Log response in the console
+    console.log(itemID);
+    // Create panel variable connected to index.html
+    let panel = d3.select("#sample-metadata");
+    // Clear metadata when option changes
+    panel.html("");
+    // Create for loop to iterate through each item in the itemID array
+    for (key in itemID) {
+        // Append the values associated with each dictionary key to the panel
+      panel.append("p").text(key + ": " + itemID[key]);
+    }
+  });
+};
 
-function optionChanged(newSample){
-    buildCharts(newSample)
-    buildMetaData(newSample)
-}
+// Create a function that changes panel data and charts data when dropdown option changes
+function optionChanged(newSelection){
+    buildCharts(newSelection);
+    buildMetaData(newSelection);
+};
+
+// Initialise the init function
+init();
